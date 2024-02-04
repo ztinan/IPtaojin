04b9e37ee872b4e0ce53fb781e3a322fd2ade18a
ztinan <43401059+ztinan@users.noreply.github.com>
2024-02-04 20:19:55 +0800

    Add files via upload

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { motion } from 'framer-motion'
 import Balancer from 'react-wrap-balancer'
 
@@ -5,40 +7,44 @@ import { SparkleIcon, UserSecurityIcon } from '~/assets'
 import { PeekabooLink } from '~/components/links/PeekabooLink'
 import { SocialLink } from '~/components/links/SocialLink'
 
-function PatentAgent() {
+function Developer() {
+  return (
+    <span className="group">
+      <span className="font-mono">&lt;</span>开发者
+      <span className="font-mono">/&gt;</span>
+      <span className="invisible inline-flex text-zinc-300 before:content-['|'] group-hover:visible group-hover:animate-typing dark:text-zinc-500" />
+    </span>
+  )
+}
+
+function Designer() {
+  return (
+    <span className="group relative rounded-2xl bg-black/5 p-1 dark:bg-white/5">
+      <span className="pointer-events-none absolute inset-0 border border-lime-700/90 opacity-70 group-hover:border-dashed group-hover:opacity-100 dark:border-lime-400/90">
+        <span className="absolute -left-0.5 -top-0.5 h-1.5 w-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
+        <span className="absolute -bottom-0.5 -right-0.5 h-1.5 w-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
+        <span className="absolute -bottom-0.5 -left-0.5 h-1.5 w-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
+        <span className="absolute -right-0.5 -top-0.5 h-1.5 w-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
+      </span>
+      设计师
+    </span>
+  )
+}
+
+function OCD() {
   return (
     <span className="group">
       <SparkleIcon className="mr-1 inline-flex transform-gpu transition-transform duration-500 group-hover:rotate-180" />
-      <span>专利代理师</span>
+      <span>细节控</span>
     </span>
   )
 }
 
-function Lawyer() {
+function Founder() {
   return (
     <span className="group">
       <UserSecurityIcon className="mr-1 inline-flex group-hover:fill-zinc-600/20 dark:group-hover:fill-zinc-200/20" />
-      <span>律师资格证</span>
-    </span>
-  )
-}
-
-function IPR() {
-  return (
-    <span className="group">
-      <span className="font-mono">&lt;</span>中级知识产权师
-      <span className="font-mono">/&gt;</span>
-      <span className="invisible inline-flex text-zinc-300 before:content-['|'] group-hover:visible group-hover:animate-typing dark:text-zinc-500" />
-    </span>
-  )
-}
-
-function IPMS() {
-  return (
-    <span className="group">
-      <span className="font-mono">&lt;</span>IPMS
-      <span className="font-mono">/&gt;</span>
-      <span className="invisible inline-flex text-zinc-300 before:content-['|'] group-hover:visible group-hover:animate-typing dark:text-zinc-500" />
+      <span>创始人</span>
     </span>
   )
 }
@@ -57,9 +63,9 @@ export function Headline() {
           duration: 0.3,
         }}
       >
-        <PatentAgent />，<Lawyer />，
+        <Developer />，<Designer />，
         <br />
-        <IPR />，<IPMS />
+        <OCD />，<Founder />
       </motion.h1>
       <motion.p
         className="mt-6 text-base text-zinc-600 dark:text-zinc-400"
@@ -74,12 +80,56 @@ export function Headline() {
         }}
       >
         <Balancer>
-          我是 嘲讽南北，
-          <PeekabooLink href="https://zolplay1.com" aria-label="关注我"></PeekabooLink>
-          极度渴望成功，愿付非凡努力。
+          我是 Cali，
+          <PeekabooLink href="https://zolplay.com">佐玩</PeekabooLink>
+          创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。
+          我热爱开发，设计，创新，享受生活，以及在未知领域中探索。
         </Balancer>
       </motion.p>
-      {/* 社交链接部分已移除 */}
+      <motion.div
+        className="mt-6 flex gap-6"
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          type: 'spring',
+          damping: 50,
+          stiffness: 90,
+          duration: 0.35,
+          delay: 0.25,
+        }}
+      >
+        <SocialLink
+          href="https://cali.so/twitter"
+          aria-label="我的推特"
+          platform="twitter"
+        />
+        <SocialLink
+          href="https://cali.so/youtube"
+          aria-label="我的 YouTube"
+          platform="youtube"
+        />
+        <SocialLink
+          href="https://cali.so/bilibili"
+          aria-label="我的 Bilibili"
+          platform="bilibili"
+        />
+        <SocialLink
+          href="https://cali.so/github"
+          aria-label="我的 GitHub"
+          platform="github"
+        />
+        <SocialLink
+          href="https://cali.so/tg"
+          aria-label="我的 Telegram"
+          platform="telegram"
+        />
+        <SocialLink href="/feed.xml" platform="rss" aria-label="RSS 订阅" />
+        <SocialLink
+          href="mailto:hi@cali.so"
+          aria-label="我的邮箱"
+          platform="mail"
+        />
+      </motion.div>
     </div>
   )
 }
