b6bdfeb302ee0950b76a0c99ed2a01fa616a92d4
ztinan <43401059+ztinan@users.noreply.github.com>
2024-02-10 11:15:15 +0800

    Update Headline.tsx

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -26,7 +26,7 @@ function Designer() {
         <span className="absolute -bottom-0.5 -left-0.5 h-1.5 w-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
         <span className="absolute -right-0.5 -top-0.5 h-1.5 w-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
       </span>
-      知产淘金
+      IP淘金
     </span>
   )
 }
